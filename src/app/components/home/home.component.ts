9c7a48a48748fe3ced5f7ebb59582d5cd30c3ddc
AndreiGiosanu00 <andreigiosanu0@gmail.com>
2022-05-29 15:43:24 +0300

    Add slide animation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,6 +16,27 @@ import {animate, sequence, style, transition, trigger} from "@angular/animations
           animate(400, style({ opacity: 0 }))
         ])
       ]
+    ),
+    trigger(
+      'whyAnimation', [
+        transition(':enter', [
+          style({ opacity: 0 }),
+          animate(1500, style({ opacity: 1 }))
+        ]),
+        transition(':leave', [
+          animate(1500, style({ opacity: 0 }))
+        ])
+      ]
+    ),
+    trigger('slideInOut', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)'}),
+          animate('400ms ease-in', style({transform: 'translateX(0%)'}))
+        ]),
+        transition(':leave', [
+          animate('400ms ease-in', style({transform: 'translateX(100%)'}))
+        ])
+      ]
     )
   ],
   templateUrl: './home.component.html',
@@ -31,14 +52,14 @@ export class HomeComponent implements OnInit {
   @HostListener('window:scroll', ['$event'])
   onWindowScroll($event: any) {
     const numb = window.scrollY;
-    if (numb >= 650 && numb < 900){
+    if (numb >= 650 && numb < 1400){
       this.scrolled = 1;
     }
     else {
-      if (numb >= 900 && numb < 1150){
+      if (numb >= 1400 && numb < 2000){
         this.scrolled = 2;
       } else {
-        if (numb >= 1150) {
+        if (numb >= 2000) {
           this.scrolled = 3;
         }
       }
